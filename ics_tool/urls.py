7b548475dd62146b777986366be23247b0b21793
mariajayraj <33465551+mariajayraj@users.noreply.github.com>
2018-08-26 22:54:51 -0700

    Update urls.py

--- ics_tool/urls.py
+++ ics_tool/urls.py
@@ -20,6 +20,7 @@ urlpatterns = [
     url(r'^add_items', views.add_items, name='add_items'),
     url(r'^add_events', views.add_events, name='add_events'),
     url(r'^donor_report', views.donor_report, name='donor_report'),
+    url(r'^donation_report', views.donation_report, name='donation_report'),
 
 ]
 
